eba1880a19d90968c7f98de729de9ec6310689f3
jxu <7989982+jxu@users.noreply.github.com>
2025-02-17 00:05:54 -0500

    Add Move class start

--- board.py
+++ board.py
@@ -11,14 +11,18 @@ PIECE_MAP = {'P': PAWN, 'N': KNIGHT, 'B': BISHOP, 'R': ROOK,
              'Q': QUEEN, 'K': KING}
 
 
-
 # 0x88 board coordinate transformations (all 0-indexed)
 # rank index 0-7 encodes ranks 1-8
 # file index 0-7 encodes files a-h
-def sqind(rank07, file07):
+def sqind(rank07: int, file07: int) -> int:
     return 16 * rank07 + file07
 
 
+def sqvalid(ind):
+    return ind & 0x88 == 0  # the magic
+
+
+# needed?
 def sqrank(ind):
     return ind & 0x7
 
@@ -28,30 +32,35 @@ def sqfile(ind):
     return ind >> 4
 
 
+def sqname(ind: int) -> str:
+    """Get algebraic coordinates from square index."""
+    assert sqvalid(ind)
+    return "abcdefgh"[sqrank(ind)] + str(sqfile(ind) + 1)
+
+
 class Position:
     """Holds all information to set up a chess position, like FEN.
 
     0x88 board
     the other half of the board is garbage, for boundary checking
-    the 0-indexed rank and file are indexed as 0b0rrr0fff
+    the 0-indexed rank and file are indexed as %0rrr0fff
+
 
        a  b  c  d  e  f  g  h
     8 70 71 72 73 74 75 76 77|78 79 7A 7B 7C 7D 7E 7F
-    7 60 61 ...              |
-    6 50                     |
-    5 40                     |
-    4 30                     |
-    3 20                     |
-    2 10                     |
-    1 00                     |
+    7 60 61 62 63 64 65 66 67|
+    6 50 51 52 53 54 55 56 57|
+    5 40 41 42 43 44 45 46 47|
+    4 30 31 32 33 34 35 36 37|
+    3 20 21 22 23 24 25 26 27|
+    2 10 11 12 13 14 15 16 17|
+    1 00 01 02 03 04 05 06 07|
 
     """
 
     board = [EMPTY] * 128
     black_move = False
     castling = None  # subset of "KQkq" for now
-
-
 
     def __init__(self, fen):
         (piece_place, side, self.castling, self.ep_target,
@@ -93,6 +102,8 @@ class Position:
 
 
 
+
+
 def test_fen():
     start_pos = \
         Position("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1")
@@ -101,7 +112,7 @@ def test_fen():
 
     for r in range(8):
         for f in range(8):
-            piece = start_pos.board[sqind(r,f)]
+            piece = start_pos.board[sqind(r, f)]
             if r == 0:
                 assert piece == rank0[f]
             elif r == 1:
@@ -118,3 +129,14 @@ def test_fen():
     assert start_pos.halfmove == 0
     assert start_pos.movecounter == 1
 
+
+def test_sqvalid():
+    assert sqvalid(0x00)
+    assert sqvalid(0x77)
+    assert not sqvalid(0x80)
+    assert not sqvalid(-1)
+
+
+def test_sqname():
+    assert sqname(0x00) == "a1"
+    assert sqname(0x77) == "h8"
---
+++ move.py
@@ -0,0 +1,28 @@
+from board import *
+
+class Move:
+    """Encode move with from square, to square, and flags"""
+    def __init__(self, from_ind, to_ind, flags=0):
+        assert sqvalid(from_ind)
+        assert sqvalid(to_ind)
+        assert from_ind != to_ind  # null move
+
+        self.from_ = from_ind
+        self.to = to_ind
+        self.flags = flags
+
+    # TODO: construct from coordinate notation string?
+
+
+    def __str__(self):
+        """Return pure algebraic coordinate notation, like h7h8q"""
+
+        # TODO: implement promotions
+        if self.flags:
+            raise NotImplementedError
+
+        return sqname(self.from_) + sqname(self.to)
+
+
+def test_move_str():
+    assert str(Move(0x00, 0x77)) == "a1h8"
