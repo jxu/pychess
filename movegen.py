00ee5be7205458c3e9e2027157af5d1144f5c0ed
jxu <7989982+jxu@users.noreply.github.com>
2025-12-29 16:01:33 -0500

    Restart movegen

---
+++ movegen.py
@@ -0,0 +1,267 @@
+from board import *
+
+class Direction(IntEnum):
+    N = 16
+    E = 1
+    S = -16
+    W = -1
+    NE = N + E
+    SE = S + E
+    SW = S + W
+    NW = N + W
+
+    # knight moves
+    NNE = N + NE
+    NNW = N + NW
+    ENE = E + NE
+    ESE = E + SE
+    SSE = S + SE
+    SSW = S + SW
+    WNW = W + NW
+    WSW = W + SW
+
+Dir = Direction
+
+PIECE_DIR = {
+    PieceType.ROOK: (Dir.N, Dir.E, Dir.S, Dir.W),
+    PieceType.BISHOP: (Dir.NE, Dir.SE, Dir.SW, Dir.NW),
+    PieceType.KNIGHT: (Dir.NNE, Dir.NNW, Dir.ENE, Dir.ESE,
+                       Dir.SSE, Dir.SSW, Dir.WNW, Dir.WSW)
+}
+PIECE_DIR[PT.QUEEN] = (PIECE_DIR[PT.ROOK] + PIECE_DIR[PT.BISHOP])
+# king moves in the same directions as queen
+PIECE_DIR[PT.KING] = PIECE_DIR[PT.QUEEN]
+
+
+def stepper_attacks(sq: square, piece_type: PieceType):
+    """Generate attacked squares of pieces that don't depend on occupancy
+    (king, knight)
+
+    Pawn depends on color.
+
+    Bitboard style: Attacks are regardless of
+    - what is actually on square
+    - what is occupying the targets
+    - legality
+
+    Move generation is left to another function.
+    """
+    if piece_type not in (PT.KNIGHT, PT.KING):
+        raise ValueError
+
+    for direction in PIECE_DIR[piece_type]:
+        step_square = sq + direction
+        if sq_valid(step_square):
+            yield step_square
+
+# # TODO: MAJOR OVERHAUL
+# def generate_piece_attacks(self, side) -> Iterator[Move]:
+#     """Generate ALL (pseudo-legal) attacks by side
+#
+#     Includes sliders (rook, bishop, queen) and steppers (king without castling, knight)
+#     """
+#
+#     piece = self.board[orig_sq]
+#     piece_type = get_piece_type(piece)
+#
+#     if piece_type == EMPTY:
+#         return
+#
+#     # Generate no moves for not my side to move
+#     if get_color(piece) != self.side:
+#         return
+#
+#     if piece_type == PAWN:
+#         yield from self.generate_pawn(orig_sq)
+#         return
+#
+#
+#     # piece is slider or stepper
+#     is_stepper = piece_type in (KING, KNIGHT)
+#
+#     directions = PIECE_DIR[piece_type]
+#
+#     for direction in directions:
+#         sq = orig_sq + direction  # start with one step already
+#         while sq_valid(sq):
+#             move = Move(orig_sq, sq)
+#             if self.board[sq] == EMPTY:
+#                 yield move
+#                 sq += direction
+#
+#                 if is_stepper:
+#                     break
+#
+#             # same color, end here
+#             elif get_color(self.board[sq]) == get_color(self.board[orig_sq]):
+#                 break
+#             else:  # enemy
+#                 yield move
+#                 break
+#
+#
+# def generate_pawn(self, sq: int):
+#     """Generate psuedo-legal pawn movement"""
+#     piece = self.board[sq]
+#     assert get_piece_type(piece) == PAWN
+#     assert sq_row(sq) not in (0, 7)  # illegal pawn position
+#
+#     # handle both colors at once
+#
+#     direction = NN if get_color(piece) == WHITE else SS
+#     capture_dirs = (NE, NW) if get_color(piece) == WHITE else (SE, SW)
+#     home_row = 1 if get_color(piece) == WHITE else 6
+#     END_ROWS = (0, 7)
+#
+#     # try single step, including possible promotion
+#     step_sq = sq + direction
+#     # sq_valid test not needed because pawn can't be in rows 0 or 7
+#     if self.board[step_sq] == EMPTY:
+#         # promotions
+#         if sq_row(step_sq) in END_ROWS:
+#             for promo in (KNIGHT, BISHOP, ROOK, QUEEN):
+#                 yield Move(sq, step_sq, promotion=promo)  # record promo
+#
+#         else:
+#             yield Move(sq, step_sq)
+#
+#
+#     # try double step from home row if possible
+#     if sq_row(sq) == home_row:
+#         step2_sq = step_sq + direction
+#         # both squares in front must be empty
+#         if self.board[step_sq] == EMPTY and self.board[step2_sq] == EMPTY:
+#             yield Move(sq, step2_sq, double_pawn_push=True)
+#
+#     # try capture (including en passant, based on position's target ep square)
+#     for dir in capture_dirs:
+#         capture_sq = sq + dir
+#         if sq_valid(capture_sq):
+#             capture_piece = self.board[capture_sq]
+#             if (capture_piece != EMPTY and
+#                  get_color(piece) != get_color(capture_piece)):
+#
+#                 # TODO: consolidate with previous promo yield
+#                 if sq_row(capture_sq) in END_ROWS:
+#                     for promo in (KNIGHT, BISHOP, ROOK, QUEEN):
+#                         yield Move(sq, capture_sq, promotion=promo)  # record promo
+#
+#                 else:
+#                     yield Move(sq, capture_sq, capture=True)
+#
+#             # EP capture
+#             if capture_sq == self.ep_target:
+#                 yield Move(sq, capture_sq, capture=True)
+#
+#
+#
+# def is_attacked(self, sq):
+#     """Determine if square (possibly empty) is attacked by enemy piece
+#     Enemy is determined by self's side
+#
+#     (would be much more efficient with bitboards!)
+#     """
+#     assert sq_valid(sq)
+#
+#     attacker_color = invert_color(self.side)
+#
+#     # loop through whole board
+#     for i in range(BOARD_SIZE):
+#         if not sq_valid(i): continue
+#
+#         if get_color(self.board[i]) == attacker_color:
+#             attacks = self.generate_piece_attacks(i)
+#             for move in attacks:
+#                 if move.to == sq:
+#                     return True
+#
+#     return False
+#
+#
+# def generate_castle(self):
+#     """Generate possible castling moves (by side to move) in the position.
+#
+#     To castle:
+#     - Must have castling rights (tracked seperately by Position object)
+#     - Must have empty spaces in between
+#     - Cannot castle out of, through, or into check
+#       - Not included even for pseudo-legal
+#     """
+#
+#     # arrays in order of WK, WQ, BK, BQ
+#     KING_SQUARES = [(E1, F1, G1), (E1, D1, C1), (E8, F8, G8), (E8, D8, C8)]
+#
+#     IN_BETWEEN = [(F1, G1), (D1, C1, B1), (F8, G8), (D8, C8, B8)]
+#     KING_COLOR = (WHITE, WHITE, BLACK, BLACK)
+#
+#     for i in range(4):
+#         # only generate moves for side to move
+#         if KING_COLOR[i] != self.side:
+#             continue
+#
+#         # castling rights
+#         if self.castling[i]:
+#             assert get_piece_type(self.board[KING_SQUARES[i][0]]) == KING
+#             # squares empty
+#             if all(self.board[s] == EMPTY for s in IN_BETWEEN[i]):
+#                 # king not in check
+#                 if all(not self.is_attacked(s) for s in KING_SQUARES[i]):
+#                     yield Move(KING_SQUARES[i][0], KING_SQUARES[i][-1], castle=True)
+#
+#
+# def make_move(self, move: Move):
+#     """Make pseudo-legal move, updating Position flags
+#
+#     See https://www.chessprogramming.org/Forsyth-Edwards_Notation
+#     for how position is stored
+#     """
+#
+#     # ensure to square doesn't have piece of same color
+#     assert get_color(self.board[move.to]) != self.side
+#
+#     # piece to move
+#     piece = self.board[move.from_]
+#     # piece should be side to move color
+#     assert get_color(piece) == self.side
+#
+#     # do board update: from square is vacated, to square is replaced
+#     self.board[move.from_] = EMPTY
+#     self.board[move.to] = piece
+#
+#
+#     # castling rights are based on if kings and rooks have moved
+#     # (left their from square) or been captured
+#
+#     # TODO: make more robust without assuming indices WK, WQ, BK, BQ
+#     ROOK_SQUARE = (H1, A1, H8, A8)
+#     ROOK_PIECE  = (ROOK, ROOK, -ROOK, -ROOK)
+#     KING_SQUARE = (E1, E1, E8, E8)
+#     KING_PIECE  = (KING, KING, -KING, -KING)
+#
+#     for i in range(4):
+#         if (self.board[ROOK_SQUARE[i]] != ROOK_PIECE[i] or
+#                 self.board[KING_SQUARE[i]] != KING_PIECE[i]):
+#             self.castling[i] = False
+#
+#     # EP target set always if double pawn push (assume valid)
+#     if move.double_pawn_push:
+#         row = sq_row(move.to)
+#         # EP is set behind to_square
+#         new_row = row - 1 if get_color(piece) == WHITE else row + 1
+#         self.ep_target = sq_index(new_row, sq_col(move.to))
+#     else:
+#         self.ep_target = None
+#
+#     # Halfmove clock reset to zero after a capture or pawn move,
+#     # increment otherwise
+#     if move.capture or get_piece_type(piece) == PAWN:
+#         self.halfmove = 0
+#     else:
+#         self.halfmove += 1
+#
+#     # Fullmove counter increments on black's move
+#     if self.side == BLACK:
+#         self.fullmove += 1
+#
+#     # finally, invert side to move
+#     self.side = invert_color(self.side)
--- test_movegen.py
+++ test_movegen.py
@@ -1,2 +1,176 @@
+from movegen import *
+
+def test_knight():
+    targets = stepper_attacks(SQ.D5, PieceType.KNIGHT)
+    assert sorted(targets) == [
+        SQ.C3, SQ.E3, SQ.B4, SQ.F4, SQ.B6, SQ.F6, SQ.C7, SQ.E7
+    ]
+
+    targets = stepper_attacks(SQ.H1, PieceType.KNIGHT)
+    assert sorted(targets) == [SQ.F2, SQ.G3]
+
+
+#
+# def moves_as_str(moves):
+#     """Return moveslist as list of algebraic UCI move strings"""
+#     return sorted([str(m) for m in moves])
+#
+#
+# def test_rook():
+#     pos = Position("8/2K5/8/2R2P2/8/8/2r5/2k5 w - - 0 1")
+#     moves = pos.generate_piece_attacks(SQ("c5"))  # c5 white rook
+#
+#
+#
+#
+# def test_bishop():
+#     pos = Position("8/4P3/8/2Bk4/2b5/8/5p2/5K2 w - - 0 1")
+#     moves = pos.generate_piece_attacks(SQ("c5"))  # white bishop
+#
+#
+#
+# def test_queen():
+#     pos = Position("8/8/8/3Q4/8/8/8/8 w - - 0 1")
+#     moves = pos.generate_piece_attacks(SQ("d5"))
+#
+#
+# def test_king_step():
+#     # doesn't include castling!
+#     pos = Position("8/8/8/3K4/6k1/8/8/8 w - - 0 1")
+#     moves = pos.generate_piece_attacks(SQ("d5"))
+#
+#
+
+#
+#
+#
+# def test_pawn():
+#     pos = Position("1k3b2/6P1/8/4pPpP/3p4/1P2N3/1PPP4/1K6 w - e6 0 1")
+#
+#     # single or double push
+#     assert moves_as_str(pos.generate_piece_attacks(SQ("c2"))) == ["c2c3", "c2c4"]
+#     assert moves_as_str(pos.generate_piece_attacks(SQ("b2"))) == []
+#
+#
+#     assert moves_as_str(pos.generate_piece_attacks(SQ("d2"))) == ["d2d3"]
+#     # e.p.
+#     assert moves_as_str(pos.generate_piece_attacks(SQ("f5"))) == ["f5e6", "f5f6"]
+#     assert moves_as_str(pos.generate_piece_attacks(SQ("h5"))) == ["h5h6"]
+#
+#
+#     # promotions
+#     assert moves_as_str(pos.generate_piece_attacks(SQ("g7"))) == \
+#            ["g7f8b", "g7f8n", "g7f8q", "g7f8r",
+#     "g7g8b", "g7g8n", "g7g8q", "g7g8r"]
+#
+#     # black pawn
+#     pos.side = BLACK
+#     assert moves_as_str(pos.generate_piece_attacks(SQ("d4"))) == ["d4d3", "d4e3"]
+#
+#     # EP is pseudo-legal here, but not legal
+#     pos = Position("8/6bb/8/8/R1pP2k1/4P3/P7/K7 b - - 0 10")
+#     assert moves_as_str(pos.generate_pawn(C4)) == ["c4c3"]
+#
+#
+# def test_is_attacked():
+#     # Test only for castling
+#     pos = Position("r3kN1r/1K6/8/8/2B1R3/8/8/8 b kq - 0 1")
+#
+#     assert not pos.is_attacked(SQ("f8"))  # occupied but not attacked
+#     assert pos.is_attacked(SQ("e8"))  # king in check
+#     assert pos.is_attacked(SQ("g8"))
+#     assert pos.is_attacked(SQ("b8"))
+#     assert pos.is_attacked(SQ("c8"))
+#     assert not pos.is_attacked(SQ("d8"))
+#
+#
+# def test_castle():
+#     # starting position
+#     pos = Position(START_FEN)
+#     assert list(pos.generate_castle()) == []
+#
+#
+#     # all castling position
+#     pos = Position("r3k2r/8/8/8/8/8/8/R3K2R w KQkq - 0 1")
+#
+#     moves = pos.generate_castle()
+#     # white castling
+#     assert moves_as_str(moves) == ["e1c1", "e1g1"]
+#     # black castling
+#     pos.side = BLACK
+#     assert moves_as_str(pos.generate_castle()) == ["e8c8", "e8g8"]
+#
+#     # can't castle out of check
+#     pos = Position("r3k2r/8/2Q5/8/8/8/8/4K3 b kq - 0 1")
+#     assert list(pos.generate_castle()) == []
+#
+#     # can't castle through/into check
+#     # kingside through check, queenside is ok here
+#     pos = Position("r3k2r/8/8/8/5R2/8/8/4K3 b kq - 0 1")
+#     assert moves_as_str(pos.generate_castle()) == ["e8c8"]
+#
+#     # queenside through check, kingside is ok
+#     pos = Position("r3k2r/8/8/8/2R5/8/8/4K3 b kq - 0 1")
+#     assert moves_as_str(pos.generate_castle()) == ["e8g8"]
+#
+#     # b8 is attacked but king doesn't go through
+#     pos = Position("r3k2r/8/8/8/1Q6/8/8/4K3 b kq - 0 1")
+#     assert moves_as_str(pos.generate_castle()) == ["e8c8"]
+#
+#
+# def test_make_move():
+#     # Assume moves are all valid
+#     pos = Position(START_FEN)
+#
+#     # 1. e4
+#     pos.make_move(Move(E2, E4, double_pawn_push=True))
+#     # pawn actually moved
+#     assert pos.board[E2] == EMPTY
+#     assert pos.board[E4] == PAWN
+#
+#     # switched sides after move
+#     assert pos.side == BLACK
+#
+#     # castling rights preserved
+#     assert pos.castling == [True]*4
+#     # ensure EP square is marked even if it's not possible
+#     assert pos.ep_target == E3
+#     # halfmove clock reset to zero after pawn move!
+#     assert pos.halfmove == 0
+#     # fullmove counter not incremented
+#     assert pos.fullmove == 1
+#
+#     # 1. ...c5
+#     pos.make_move(Move(C7, C5, double_pawn_push=True))
+#
+#     assert pos.board[C7] == EMPTY
+#     assert pos.board[C5] == -PAWN
+#     assert pos.side == WHITE
+#     assert pos.castling == [True]*4
+#     assert pos.ep_target == C6
+#     assert pos.halfmove == 0
+#     assert pos.fullmove == 2
+#
+#     # 2. ...Nf3
+#     pos.make_move(Move(G1, F3))
+#
+#     assert pos.board[G1] == EMPTY
+#     assert pos.board[F3] == KNIGHT
+#     assert pos.side == BLACK
+#     assert pos.castling == [True]*4
+#     assert pos.ep_target == None
+#     assert pos.halfmove == 1
+#     assert pos.fullmove == 2
+#
+#     # Test castling rights
+#     italian_fen = "r1bqk1nr/pppp1ppp/2n5/2b1p3/2B1P3/5N2/PPPP1PPP/RNBQK2R w KQkq - 4 4"
+#     pos = Position(italian_fen)
+#     pos.make_move(Move(E1, E2))  # move king
+#     assert pos.castling == [False, False, True, True]
+#
+#     pos = Position(italian_fen)  # recreated
+#     pos.make_move(Move(H1, F1))  # move rook
+#     assert pos.castling == [False, True, True, True]
+#
 
 
